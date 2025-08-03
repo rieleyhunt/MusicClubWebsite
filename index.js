83c130f7c585bf56d574a6d86e74b562f95b22c6
Rieley <huntrieley@gmail.com>
2025-08-03 13:52:30 -0800

    sdfa

--- index.js
+++ index.js
@@ -141,5 +141,5 @@ app.post('/concerts', async (req, res) => {
 });
 
 // ----- Start server -----
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`API on :${PORT}`));
